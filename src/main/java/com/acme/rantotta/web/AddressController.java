59f130aca8e0ed6c783deab998bf942a9ba91044
lalyos <lalyos@yahoo.com>
2013-12-02 17:05:11 +0100

    @Validated annotation kicks JSR-303

--- src/main/java/com/acme/rantotta/web/AddressController.java
+++ src/main/java/com/acme/rantotta/web/AddressController.java
@@ -4,6 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.acme.rantotta.domain.Address;
@@ -17,15 +19,14 @@ public class AddressController {
     }
     
     @RequestMapping("/address/set")
-    public String set(HttpServletRequest request, HttpSession session) {
-        Address address = new Address();
-        
-        address.setCity(request.getParameter("city"));
-        address.setStreet(request.getParameter("street"));
-        address.setZip(request.getParameter("zip"));
-        
-        session.setAttribute("address", address);
-        return "redirect:/address";
+    public String set(HttpServletRequest request, HttpSession session, 
+            @Validated Address address, BindingResult result) {
+        if (result.hasErrors()) {
+            return "address/show";
+        } else {
+            session.setAttribute("address", address);
+            return "redirect:/address";
+        }
     }
     
 }
