4988b4540820655877d203f0ec1eecd52913d86f
Csaba Valyi <csaba_valyi@epam.com>
2013-12-02 14:56:30 +0100

    breakTime delete implemented

--- src/main/java/com/acme/rantotta/web/BreakController.java
+++ src/main/java/com/acme/rantotta/web/BreakController.java
@@ -1,14 +1,37 @@
 package com.acme.rantotta.web;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.acme.rantotta.service.BreakTimeService;
 
 @Controller
 public class BreakController {
 
+    private final BreakTimeService service;
+    
+    @Autowired
+    public BreakController(BreakTimeService service){
+        this.service = service;
+    }
+    
     //kontroller metódusa view névvel tér vissza
     @RequestMapping("/list")
-    public String list(){
+    public String list(Model model){
+        model.addAttribute("breakList", service.getAll());
+        model.addAttribute("minutes", service.getMinutesTillNext());
         return "list";
+    }
+    
+    @RequestMapping("/delete")
+    public String delete(HttpServletRequest request){
+        String breakToDelete = request.getParameter("break");
+        service.delete(breakToDelete);
+        
+        return "redirect:/list";
     }
 }
