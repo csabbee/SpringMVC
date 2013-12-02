b69f9794063a4a9509722b608605787d2d7c1b64
lalyos <lalyos@yahoo.com>
2013-12-02 14:19:10 +0100

    break controller list() implemented

--- src/main/java/com/acme/rantotta/web/BreakController.java
+++ src/main/java/com/acme/rantotta/web/BreakController.java
@@ -1,13 +1,26 @@
 package com.acme.rantotta.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.acme.rantotta.service.BreakTimeService;
 
 @Controller
 public class BreakController {
 
+    private final BreakTimeService service;
+    
+    @Autowired
+    public BreakController(BreakTimeService service) {
+        this.service = service;
+    }
+    
     @RequestMapping("/list")
-    public String list() {
+    public String list(Model model) {
+        
+        model.addAttribute("breakList", service.getAll());
         return "list";
     }
 }
--- src/main/webapp/WEB-INF/views/list.jsp
+++ src/main/webapp/WEB-INF/views/list.jsp
@@ -1,4 +1,9 @@
+<%@taglib uri="http://java.sun.com/jsp/jstl/core"  prefix="c"%>
 <h2>list</h2>
 
+<ul>
+  <c:forEach var="next" items="${breakList}">
+    <li>${next }</li>
+  </c:forEach>  
+</ul>
 
-todo...
