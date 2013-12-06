840f7561d44dd52c049570786b6c31756a14c09e
Csaba Valyi <csaba_valyi@epam.com>
2013-12-06 16:26:45 +0100

    sessionId map in OrderService

--- src/main/java/com/acme/rantotta/service/OrderService.java
+++ src/main/java/com/acme/rantotta/service/OrderService.java
@@ -1,9 +1,11 @@
 package com.acme.rantotta.service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,8 @@ import com.acme.rantotta.order.Order;
 public class OrderService {
 
     private Map<String, Order> orderMap = new TreeMap<String, Order>();
+    private Map<String, Integer> sessionIdMap = new HashMap<String, Integer>();
+    private AtomicInteger cartId = new AtomicInteger(250);
     
     public void addOrder(Order order){
         orderMap.put(order.getOrderId(), order);
@@ -26,4 +30,11 @@ public class OrderService {
     public List<Order> getAll(){
         return new ArrayList<Order>(orderMap.values());
     }
+    public Integer getCartIdBySessionId(String sessionId){
+        if(!sessionIdMap.containsKey(sessionId)){
+            sessionIdMap.put(sessionId, cartId.getAndIncrement());
+        }
+        return sessionIdMap.get(sessionId);
+    }
+    
 }
