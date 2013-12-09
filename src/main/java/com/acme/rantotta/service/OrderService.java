7b7b7a4b7752b6ce53516774e8dee20584dd8f81
Csaba Valyi <csaba_valyi@epam.com>
2013-12-09 15:27:56 +0100

    [refactor] FoodService injected into OrderService

--- src/main/java/com/acme/rantotta/service/OrderService.java
+++ src/main/java/com/acme/rantotta/service/OrderService.java
@@ -7,16 +7,25 @@ import java.util.Map;
 import java.util.TreeMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.acme.rantotta.domain.Food;
 import com.acme.rantotta.order.Order;
 
 @Service
 public class OrderService {
 
-    private Map<String, Order> orderMap = new TreeMap<String, Order>();
+    private Map<Integer, Order> orderMap = new TreeMap<Integer, Order>();
     private Map<String, Integer> sessionIdMap = new HashMap<String, Integer>();
     private AtomicInteger cartId = new AtomicInteger(250);
+    private FoodService foodService;
+    
+    @Autowired
+    public OrderService(FoodService foodService) {
+        super();
+        this.foodService = foodService;
+    }
     
     public void addOrder(Order order){
         orderMap.put(order.getOrderId(), order);
@@ -24,8 +33,17 @@ public class OrderService {
     public void deleteById(String id){
         orderMap.remove(id);
     }
-    public Order findById(String id){
-        return orderMap.get(id);
+    public Order getOrderById(Integer cartId){
+        if(sessionIdMap.containsValue(cartId)){
+            if (!orderMap.containsKey(cartId)) {
+                Order order = new Order();
+                order.setOrderId(cartId);
+                orderMap.put(cartId, order);
+            }
+        } else {
+            throw new IllegalArgumentException("Cartid does not exist! You have to call POST /cart first");
+        }
+        return orderMap.get(cartId);
     }
     public List<Order> getAll(){
         return new ArrayList<Order>(orderMap.values());
@@ -36,5 +54,7 @@ public class OrderService {
         }
         return sessionIdMap.get(sessionId);
     }
-    
+    public Food getFoodById(String foodId){
+        return foodService.find(foodId);
+    }
 }
