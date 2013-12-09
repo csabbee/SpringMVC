9fa26e643a1d4687ad897b6cd218d97da2a64c70
Csaba Valyi <csaba_valyi@epam.com>
2013-12-09 15:27:08 +0100

    put food to order implemented

--- src/main/java/com/acme/rantotta/web/OrderController.java
+++ src/main/java/com/acme/rantotta/web/OrderController.java
@@ -1,5 +1,7 @@
 package com.acme.rantotta.web;
 
+import java.io.IOException;
+import java.net.URLDecoder;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -8,17 +10,25 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.acme.rantotta.order.Order;
+import com.acme.rantotta.order.OrderItem;
 import com.acme.rantotta.service.OrderService;
 
 @Controller
@@ -62,6 +72,34 @@ public class OrderController {
         for (String cookie : cookies) {
             cookieMap.put(cookie.toString().split("=")[0], cookie.toString().split("=")[1]);
         }
-        return "{\"id\":\"" +service.getCartIdBySessionId(cookieMap.get("JSESSIONID"))+ "\"}";
+        
+        return "{\"id\":" +service.getCartIdBySessionId(cookieMap.get("JSESSIONID"))+ "}";
+    }
+    
+    @RequestMapping(value="/cart/{cartId}", method=RequestMethod.PUT)
+    @ResponseStatus(value=HttpStatus.ACCEPTED)
+    public String putOrderItemInOrder(@PathVariable Integer cartId, @ModelAttribute String foodId, 
+            @ModelAttribute int quantity){
+        Order order = service.getOrderById(cartId);
+        OrderItem orderItem = new OrderItem(service.getFoodById(foodId).getId());
+        orderItem.setQuantity(quantity);
+        order.addOrderItemToOrder(orderItem);
+        return "/food";
+    }
+    
+    @RequestMapping(value="/cart/{cartId}", method=RequestMethod.PUT, produces=PRODUCES_JSON)
+    @ResponseStatus(value=HttpStatus.ACCEPTED)
+    @ResponseBody
+    public String putOrderItemInOrderFromJson(@PathVariable Integer cartId, 
+            @RequestBody String json) throws JsonParseException, JsonMappingException, IOException{
+        logger.warn("cartId: {}", cartId);
+        String decodedJson = URLDecoder.decode(json);
+        logger.warn("decodedJson: {}", decodedJson);
+        ObjectMapper mapper = new ObjectMapper();
+        OrderItem orderItem = mapper.readValue(decodedJson, OrderItem.class);
+        logger.warn("orderitem: {}", orderItem);
+        Order order = service.getOrderById(cartId);
+        order.addOrderItemToOrder(orderItem);
+        return "{\"status\":\"ok\"}";
     }
 }
